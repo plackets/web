e912d1bf3da6e74fadc89a946595ab05dee79d39
hectorin19 <hectortobariie@gmail.com>
2020-12-21 04:43:22 -0300

    edit en models.py

--- ventas/__manifest__.py
+++ ventas/__manifest__.py
@@ -3,33 +3,32 @@
     'name': "ventas",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        ventas de gustoso""",
 
     'description': """
-        Long description of module's purpose
+        modulo que gestiona las ventas del restaurant gustoso
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "hectorin19",
+    'website': "http://www.utalca.cl",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': '',
     'version': '0.1',
 
-    # any module necessary for this one to work correctly
-    'depends': ['base'],
+    # aquí se coloca de que modulo depende este 
+    'depends': ['base'], 
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        # 'security/ir.model.access.csv', que datos voy a ocupar
+        #'views/views.xml',
+        #'views/templates.xml',
     ],
     # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+   # 'demo': [
+    #    'demo/demo.xml',
+    #],
 }
