81d199af2cb14f9136b223adbf0e253b499d79b7
plackets <morales.pipe97@gmail.com>
2020-12-21 04:15:58 -0300

    aplication true

--- inventario/__manifest__.py
+++ inventario/__manifest__.py
@@ -6,7 +6,7 @@
         plackets""",
 
     'description': """
-        Sistema de inventario
+        Proyecto web
     """,
 
     'author': "Alumnos IIE",
@@ -15,7 +15,7 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Inventario',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
@@ -31,4 +31,5 @@
     'demo': [
         'demo/demo.xml',
     ],
+    'application': True,
 }
