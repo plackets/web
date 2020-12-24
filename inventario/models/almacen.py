48218701c954dbac8a3f44c306ae8ec8f752f452
plackets <morales.pipe97@gmail.com>
2020-12-24 14:05:28 -0300

    Required

--- inventario/models/almacen.py
+++ inventario/models/almacen.py
@@ -21,9 +21,9 @@ class almacenamiento(models.Model):
 class inventario(models.Model):
      _name = 'inventario.almacen'
 
-     nombre = fields.Char()
-     cantidad = fields.Integer()
-     fecha = fields.Date("Fecha")
+     nombre = fields.Char(required=True)
+     cantidad = fields.Integer(required=True)
+     fecha = fields.Date("Fecha",required=True)
      porcentaje = fields.Selection([ ('0', '0'),
     ('1', '1'),('2','2'),('3','3'),('4','4'),('5','5'),('6','6'),('7','7'),('8','8'),('9','9'),
     ('10','10'),('11','11'),('12','12'),('13','13'),('14','14'),('15','15'),('16','16'),('17','17'),('18','18'),('19','19'),
@@ -37,6 +37,6 @@ class inventario(models.Model):
     ('90','90'),('91','91'),('92','92'),('93','93'),('94','94'),('95','95'),('96','96'),('97','97'),('98','98'),('99','99'),
     ('100','100')],'% de uso', default='0', required=True)
 
-     almacenamiento_id = fields.Many2one('inventario.almacenamiento', string = 'Almacenamiento')
+     almacenamiento_id = fields.Many2one('inventario.almacenamiento', string = 'Almacenamiento',required=True)
 
      
